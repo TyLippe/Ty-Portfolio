59e0893fa9ebe6024258a0c5e0af9cdcb81e8add
Ty Lippe <tylippe94@gmail.com>
2019-12-28 15:17:26 -0500

    getting started

---
+++ portfolio/src/Components/contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className='contactDiv'>
+            <button>Contact</button>
+        </div>
+    )
+}
+
+export default Contact;
