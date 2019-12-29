3543ae7c81b5f99568ded480d8c2327b5a50de9a
Ty Lippe <tylippe94@gmail.com>
2019-12-28 22:14:02 -0500

    added header and changed styling

--- portfolio/src/Components/header.jsx
+++ portfolio/src/Components/header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import ProfilePic from '../Images/Ty.png'
 
 const Header = () => {
     return(
-        <p>Header</p>
+        <div className='headerDiv'>
+            <img src={ProfilePic} alt="Ty Lippe's Avatar" />
+            <div className='headerContainer'>
+                <h1>Hey there!</h1>
+                <p>I am Ty, welcome to my React Portfolio! As a Full Stack Developer I have studied both the Front-End and the Back-End of Software Development. I studied at Lambda School, a remote coding bootcamp that teaches students how to become a successful software engineer. I currently am located in Orlando, FL and am looking to move me and my family out to the beautiful city of Nashville, TN. Feel free to look around and check out some of the awesome projects I have worked on and feel free to contact me! </p>
+            </div>
+        </div>
     )
 }
 
