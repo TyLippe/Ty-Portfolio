108151650f3efe479a0cd84fc014a6d942ca6b5e
Ty Lippe <tylippe94@gmail.com>
2020-01-07 22:16:39 -0500

    added skills

--- src/Components/skills.jsx
+++ src/Components/skills.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import skillData from '../skills';
 
 const Skills = () => {
+    console.log(skillData)
+
     return(
         <div className='skillDiv'>
             <h1>Skills</h1>
+            <div className='filter'>
+                <button>All</button>
+                <button>Programming Languages</button>
+                <button>Libraries and Frameworks</button>
+                <button>Databases</button>
+                <button>Version Control</button>
+                <button>Software Development</button>
+                <button>Communication Software</button>
+            </div>
+            <div className='skills'>
+                {skillData.map(data => {
+                    return(
+                        <li>{data.name}</li>
+                    )
+                })}
+            </div>        
         </div>
     )
 }
---
+++ src/skills.jsx
@@ -0,0 +1,104 @@
+const skillData = [
+    {
+        name: 'HTML5',
+        type: 'language'
+    },
+    {
+        name: 'CSS3',
+        type: 'language'
+    },
+    {
+        name: 'Javascript',
+        type: 'language'
+    },
+    {
+        name: 'ReactJS',
+        type: 'library'
+    },
+    {
+        name: 'Redux',
+        type: 'library'
+    },
+    {
+        name: 'Node.JS',
+        type: 'library'
+    },
+    {
+        name: 'Knex',
+        type: 'library'
+    },
+    {
+        name: 'Express',
+        type: 'library'
+    },
+    {
+        name: 'LESS/SASS',
+        type: 'library'
+    },
+    {
+        name: 'Material UI',
+        type: 'library'
+    },
+    {
+        name: 'Bootstrap',
+        type: 'library'
+    },
+    {
+        name: 'SQLite',
+        type: 'db'
+    },
+    {
+        name: 'PostgreSQL',
+        type: 'db'
+    },
+    {
+        name: 'Git',
+        type: 'version'
+    },
+    {
+        name: 'Github',
+        type: 'version'
+    },
+    {
+        name: 'Heroku',
+        type: 'version'
+    },
+    {
+        name: 'Netlify',
+        type: 'version'
+    },
+    {
+        name: 'VSCode',
+        type: 'version'
+    },
+    {
+        name: 'MacOS',
+        type: 'version'
+    },
+    {
+        name: 'Terminal',
+        type: 'version'
+    },
+    {
+        name: 'Windows',
+        type: 'version'
+    },
+    {
+        name: 'Object Oriented Programming',
+        type: 'software'
+    },
+    {
+        name: 'Pair Programming',
+        type: 'software'
+    },
+    {
+        name: 'Slack',
+        type: 'com'
+    },
+    {
+        name: 'Zoom',
+        type: 'com'
+    },
+]
+
+export default skillData;
