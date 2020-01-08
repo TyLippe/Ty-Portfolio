1d752ca4d9e4234428bfbf56779ea728bb227dac
Ty Lippe <tylippe94@gmail.com>
2020-01-08 11:40:01 -0500

    added skills functionality

--- src/Components/skills.jsx
+++ src/Components/skills.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import skillData from '../skills';
 
 const Skills = () => {
-    console.log(skillData)
+    const [ skills, setSkills ] = useState(skillData)
+
+    const filtered = (type) => {
+        let filterSkills = skillData.filter(function(data) {
+            if(data.type === type) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        setSkills(filterSkills)
+    } 
 
     return(
         <div className='skillDiv'>
             <h1>Skills</h1>
             <div className='filter'>
-                <button>All</button>
-                <button>Programming Languages</button>
-                <button>Libraries and Frameworks</button>
-                <button>Databases</button>
-                <button>Version Control</button>
-                <button>Software Development</button>
-                <button>Communication Software</button>
+                <button onClick={() => setSkills(skillData)}>All</button>
+                <button onClick={() => filtered('language')}>Programming Languages</button>
+                <button onClick={() => filtered('library')}>Libraries and Frameworks</button>
+                <button onClick={() => filtered('db')}>Databases</button>
+                <button onClick={() => filtered('version')}>Version Control</button>
+                <button onClick={() => filtered('software')}>Software Development</button>
+                <button onClick={() => filtered('com')}>Communication Software</button>
             </div>
             <div className='skills'>
-                {skillData.map(data => {
+                {skills.map(skill => {
                     return(
-                        <li>{data.name}</li>
+                        <li>{skill.name}</li>
                     )
                 })}
             </div>        
